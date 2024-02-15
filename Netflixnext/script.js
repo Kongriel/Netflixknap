15ccdc3cc8ad5e3cef9574c8b95f412e6de287fe
Kongriel <124664554+Kongriel@users.noreply.github.com>
2024-02-15 23:07:10 +0100

    Update script.js

--- Netflixnext/script.js
+++ Netflixnext/script.js
@@ -1,60 +1,47 @@
-
 let mouseMovingOverContainer = false;
 
-
+// Function to handle mousemove event over the container
 function handleMouseMoveOverContainer() {
-
   mouseMovingOverContainer = true;
-  
-  // Speed up aniamtion
+  // Set animation speed fast
   document.getElementById("nextEpisodeBtn").style.animationDuration = "0.1s";
 }
 
-
+// Function to handle mouseleave event over the container
 function handleMouseLeaveContainer() {
   mouseMovingOverContainer = false;
-  // Restore animation speed to normal when mouse leaves the container
   document.getElementById("nextEpisodeBtn").style.animationDuration = "4s";
 }
 
+// Function to handle button click event
+document.getElementById("nextEpisodeBtn").addEventListener("click", function () {
+  // Change color of the container
+  document.querySelector(".container").style.backgroundColor = getRandomColor();
+});
+
+// Function to handle animation end event
 function handleAnimationEnd() {
   // Check if mouse is not moving over the container
   if (!mouseMovingOverContainer) {
-    // Switch video and play automatically when animation ends only if mouse is not moving over the container
-    switchVideo();
+    // Change color of the container
+    document.querySelector(".container").style.backgroundColor = getRandomColor();
   }
 }
 
-
-function switchVideo() {
-  // Pause the currently playing video
-  document.querySelectorAll(".video").forEach((video) => {
-    if (!video.paused) {
-      video.pause();
-    }
-  });
-
-  // Hide all videos and show the next one
-  const videos = document.querySelectorAll(".video");
-  let nextVideoIndex = (currentVideoIndex + 1) % videos.length;
-  videos[currentVideoIndex].style.display = "none";
-  videos[nextVideoIndex].style.display = "block";
-  videos[nextVideoIndex].play(); 
-  currentVideoIndex = nextVideoIndex;
+// Random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
-// Variable to keep track of the current video index
-let currentVideoIndex = 0;
-
-// Add event listener for the button click
-document.getElementById("nextEpisodeBtn").addEventListener("click", function () {
-  // Switch to the next video
-  switchVideo();
-});
-
-// Add event listeners for mouse movements
+// event listeners for mouse movements
 document.getElementById("videoContainer").addEventListener("mousemove", handleMouseMoveOverContainer);
 document.getElementById("videoContainer").addEventListener("mouseleave", handleMouseLeaveContainer);
 
-// Add event listener for animation end
+// animation end
 document.getElementById("nextEpisodeBtn").addEventListener("animationend", handleAnimationEnd);
+
